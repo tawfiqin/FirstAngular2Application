830826969481f0dc3fc39619f710208eec323ffb
Tawfiq abu Halawah <tawfiqin@gmail.com>
2016-09-17 00:08:20 +0300

    Add project files.

---
+++ FirstAngular2Application/app/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+
+import {AppComponent} from "./components/app.component";
+import {NameComponent} from "./components/name.component"
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, NameComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FirstAngular2Application/app/components/name.component.ts
@@ -0,0 +1,13 @@
+﻿import {Component} from "@angular/core";
+@Component({
+    selector: 'name-comp',
+    templateUrl: "app/components/name-view.html"
+})
+
+export class NameComponent {
+    public name: string;
+    public welcomeOut: string;
+    public welcome() {
+        this.welcomeOut = "Salam " + this.name + " this is welcome function";
+    }
+}
